82f61934c16abcabd4c57ca83b648aaecbedb148
Fabian <fabian.sucholas123@gmail.com>
2021-07-03 22:16:03 +0200

    update

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ onload = size;
 window.onresize = size;
 
 function size() {
+    //var wielkosc = (document.body.clientHeight - document.getElementById("header").offsetHeight);
     var wielkosc = (document.body.clientHeight - document.getElementById("header").offsetHeight);
     document.getElementById("navandart").style.height = wielkosc + "px";
 }
