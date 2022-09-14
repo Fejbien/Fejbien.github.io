ef936764874bea872cfa011748713bd4dfb84072
Fabian <fabian.sucholas123@gmail.com>
2022-09-14 20:40:14 +0200

    frontpage cube

--- js/pathfinding.js
+++ js/pathfinding.js
@@ -1,27 +1,23 @@
 /// <reference path="../intelli/p5.global-mode.d.ts"/>
 /// <reference path="../intelli/p5.d.ts"/>
 
-let sizeBox
+let sizeBox = 600
+let sizeSquare = 30
 
 function setup(){
-    let cnv = createCanvas(400,400);
+    let cnv = createCanvas(sizeBox, sizeBox);
     cnv.parent('canvasLocation');
-    windowResized();
-}
-
-function windowResized() {
-    let divHeight = Math.floor(document.getElementById("canvasLocation").offsetHeight)
-    let divWidth = Math.floor(document.getElementById("canvasLocation").offsetWidth)
-
-    if(divHeight > divWidth)
-        sizeBox = divWidth
-    else
-        sizeBox = divHeight
-
-    resizeCanvas(sizeBox, sizeBox)
 }
 
 function draw(){
     background('#495057');
-    square(30,20,55)
+
+    for(let i = 0; i < sizeBox; i = i + sizeSquare)
+        for(let j = 0; j < sizeBox; j = j + sizeSquare)
+            square(j,i,sizeSquare)
+
+    noFill()
+    strokeWeight(4);
+    stroke(51);
+    rect(0,0, sizeBox, sizeBox)
 }
